6f8e5ba3d90a28291854fbfe42ddac4f041f3bac
Luisa Fer <l.f.d.sando@gmail.com>
2023-01-20 21:47:31 -0500

    cargue Booststrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
       <h1>En construcción...</h1> 
+      <Button variant="primary">Primary</Button>
     </div>
   );
 }
